4028cfd568cdd0fc691d1a6f7b5cf7f92ddd6cb3
Vaibhav Shrivastava <shrivastava.vaibhav@thinksys.com>
2022-04-20 18:14:45 -0700

    changes Made

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,46 +1,55 @@
-import React from 'react'
-import './index.css'
-import flip from './flip.png'
-import { findAllByPlaceholderText } from '@testing-library/react'
+import React from "react";
+import "./index.css";
+import flip from "./flip.png";
+
 const index = () => {
     return (
-        <div className='login'>
-            
-                <div className='wrapping'>
-                    <div className='heading'><h1>Flipkart</h1></div>
-                    <div className='flip'><img src={flip} alt='flipkart' /></div>
+        <div className="login">
+            <div className="wrapping">
+                <div className="heading">
+                    <h1>Flipkart</h1>
+                </div>
+                <div className="flip">
+                    <img src={flip} alt="flipkart" />
+                </div>
+            </div>
 
+            <div className="page">
+                <div className="line1">
+                    <h2>
+                        <b>Log in for the best experience</b>{" "}
+                    </h2>
+                </div>
+                <div className="line2">
+                    <h2> Enter your phone number to continue</h2>
+                </div>
+                <div className="inputDialCode">
+                    <div className="dialCode">+91</div>
 
+                    <div className="inputWrapper">
+                        <input type="text" placeholder="Phone Number"></input>
+                    </div>
+                </div>
+                <div className="Email">
+                    <h5>Use Email</h5>
                 </div>
 
-                <div className='page'>
-                    <div className='line1'>
-                        <h3><b>Log in for the best experience</b> </h3>
-
+                <div className="subject">
+                    <div className="deepsubject">
+                        {" "}
+                        By continuing ,you agree to Flipkart's{" "}
+                        <span className="foot">Terms of Use </span>and{" "}
+                        <span className="foot">Privacy Policy</span>{" "}
+                        
                     </div>
-                    <div className='line2'>
-                        <h4> Enter your phone number to continue</h4>
-
-                    </div>
-                    <div className='inputdata'>
-                        <input type='text' placeholder='Phone Number'></input>
-                    </div>
-                    <div className='Email'><h5>Use Email</h5></div>
-
-
-                    <div className='subject'>
-                        <p> By continuing ,you agree to Flipkart's <span className='foot'>Terms of Use </span>and <span className='foot'>Privacy Policy</span> </p>
-                    </div>
-
-                    <div className='wrapper'>
-
-                        <button type='submit'>Complete</button>
-                    </div>
-
                 </div>
-            
+
+                <div className="wrapper">
+                    <button type="submit">Complete</button>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default index
+export default index;
